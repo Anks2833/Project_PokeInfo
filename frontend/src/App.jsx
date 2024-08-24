74ac86e9c87154f574cd601d55b59866bd1d1cb5
anks2833 <dubeyankur2833@gmail.com>
2024-08-24 14:54:51 +0530

    seventeenth commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,11 @@
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import Routing from "./utils/Routing";
 import Navbar from "./Components/Navbar";
-import Footer from "./Components/Footer";
 
 const App = () => {
   return (
     <div className="w-full h-screen">
       <Navbar />
       <Routing/>
-      {/* <Footer /> */}
     </div>
   )
 }
